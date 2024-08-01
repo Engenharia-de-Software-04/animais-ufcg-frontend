372aec389a6501f48dd0237311028dbe38085c18
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2024-08-01 20:58:00 -0300

    feat: creating login and signup forms

---
+++ src/app/admin/(auth)/login/page.js
@@ -0,0 +1,31 @@
+"user client";
+
+import React from 'react';
+
+import {
+    Flex,
+    Center,
+    FormControl,
+    FormLabel,
+    Input,
+    Button
+
+} from '@chakra-ui/react'
+
+export default function Login() {
+    return <div className="form-wrapper">
+        <Flex bg="white">
+            <Center w='100%' h="100vh">
+                <Center p="2rem" boxShadow='md' border='1px' borderColor='gray.300' borderRadius="20px">
+                    <FormControl>
+                        <FormLabel>E-mail</FormLabel>
+                        <Input mb="1rem" type='email' />
+                        <FormLabel>Password</FormLabel>
+                        <Input mb="1rem" type='password' />
+                        <Button>Send</Button>
+                    </FormControl>
+                </Center>
+            </Center>
+        </Flex>
+    </div>
+}
