538f5d244aae83dc2539e12a5d4784a76256e0cd
larysamendess <larysa.almeida.ext@synchro.com.br>
2024-09-14 18:33:39 -0300

    feat: created login screen

--- src/app/admin/(auth)/login/page.js
+++ src/app/admin/(auth)/login/page.js
@@ -1,7 +1,7 @@
 "use client";
 
-import React, { useState } from 'react';
-import { Flex, Center, FormControl, FormLabel, Input, Button, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Flex, Center, FormControl, FormLabel, Input, Button, Text, Image } from '@chakra-ui/react';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
@@ -13,7 +13,7 @@ export default function Login() {
     
         const form = event.target;
         const formData = new FormData(form);
-        const data = Object.fromEntries(formData.entries())
+        const data = Object.fromEntries(formData.entries());
     
         const result = await signIn('credentials', {
             redirect: false,
@@ -22,26 +22,61 @@ export default function Login() {
         });
 
         if (result["status"] == 200) {
-            router.push("/admin")
+            router.push("/admin");
         }
     };
 
     return (
         <div className="form-wrapper">
-            <Flex bg="white">
-                <Center w='100%' h="100vh">
-                    <Center p="2rem" boxShadow='md' border='1px' borderColor='gray.300' borderRadius="20px">
+            <Flex
+            direction="column"
+            align="center"
+            justify="center"
+            h="100vh"
+            w="100vw"
+            bgImage="url('/images/plano_fundo.png')"
+            bgSize="cover"
+            bgPosition="center"
+            p="2rem"
+            >
+                
+                <Flex direction="column" align="center" p="2rem" maxWidth="550px"  boxShadow="none" border="none" mb="0">
+                    <Flex mb="2rem" align="center" >
+                        <Image src="/images/logo.png" alt="Logo" width={150} mr="1rem" />
+                        <Text fontSize="3xl" fontWeight="bold" color="#FFB210">
+                            Animais UFCG
+                        </Text>
+                    </Flex>
+                    <Center p="2rem">
                         <form onSubmit={handleLoginAdmin}>
                             <FormControl>
-                                <FormLabel>E-mail</FormLabel>
-                                <Input mb="1rem" type='email' name='email' required />
-                                <FormLabel>Password</FormLabel>
-                                <Input mb="1rem" type='password' name='password' required />
-                                <Button type='submit'>Send</Button>
+                                
+                                <Input mb="1rem"
+                                    type='email'
+                                    name='email'
+                                    placeholder='E-mail'
+                                    required
+                                    border="1px solid"
+                                    bg="white"
+                                    borderRadius="25px"
+                                    height={50}
+                                />
+                                <Input 
+                                    mb="1rem"
+                                    type='password'
+                                    name='password'
+                                    placeholder='Senha'
+                                    required
+                                    border="1px solid"
+                                    bg="white"
+                                    borderRadius="25px"  
+                                    height={50}
+                                />
+                                <Button type='submit' size='lg' width={130} mx="auto" backgroundColor="#FFB210" display="block" borderRadius="25px"  >Entrar</Button>
                             </FormControl>
                         </form>
                     </Center>
-                </Center>
+                </Flex>
             </Flex>
         </div>
     );
