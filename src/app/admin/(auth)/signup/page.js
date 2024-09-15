652fed026ba4b5a673df2230ab6e5a26f4f80788
larysamendess <larysa.almeida.ext@synchro.com.br>
2024-09-15 18:40:19 -0300

    feat: add signup admin

--- src/app/admin/(auth)/signup/page.js
+++ src/app/admin/(auth)/signup/page.js
@@ -1,18 +1,21 @@
 "use client";
-import React from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import {
     Flex,
     Center,
     FormControl,
-    FormLabel,
     Input,
-    Button
+    Button,
+    Image,
+    Text
 } from '@chakra-ui/react';
 import { postAdmin } from '@/app/service/auth';
 
 export default function Register() {
     const router = useRouter();
+    const [error, setError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
 
     const handlePostAdmin = async (event) => {
         event.preventDefault();
@@ -20,39 +23,134 @@ export default function Register() {
         const form = event.target;
         const formData = new FormData(form);
 
+        const password = formData.get('password');
+        const password1 = formData.get('password1');
+
+        if (password.length <= 8) {
+            setPasswordError('A senha deve ter no mínimo 8 digitos');
+            return;
+        }
+
+        if (password !== password1) {
+            setError('As senhas não coincidem');
+            setPasswordError('');
+            return;
+        }
+
+        setError(''); 
+        setPasswordError(''); 
         try {
             const res = await postAdmin(formData);
             console.log(res);
             if (res.status === 200) {
-                // Redireciona para a página de login se a resposta for ok
                 router.push('/admin/login');
             } else {
-                // Trate o erro aqui se necessário
                 console.error('Erro ao registrar o administrador');
             }
         } catch (error) {
             console.error('Erro ao fazer a solicitação', error);
         }
     };
-
+   
     return (
         <div className="form-wrapper">
-            <Flex bg="white">
-                <Center w='100%' h="100vh">
-                    <Center p="2rem" boxShadow='md' border='1px' borderColor='gray.300' borderRadius="20px">
+            <Flex
+                direction="column"
+                align="center"
+                justify="center"
+                h="100vh"
+                w="100vw"
+                bgImage="url('/images/plano_fundo.png')"
+                bgSize="cover"
+                bgPosition="center"
+                p="2rem"
+            >
+                <Flex 
+                    direction="column" 
+                    align="center" 
+                    p="2rem" 
+                    maxWidth="550px"  
+                    boxShadow="none" 
+                    border="none" 
+                    mb="0"
+                >
+                    <Flex mb="2rem" align="center">
+                        <Image src="/images/logo.png" alt="Logo" width={150} mr="1rem" />
+                        <Text fontSize="3xl" fontWeight="bold" color="#FFB210">
+                            Animais UFCG
+                        </Text>
+                    </Flex>
+                    <Center p="2rem">
                         <form onSubmit={handlePostAdmin}>
                             <FormControl>
-                                <FormLabel>Name</FormLabel>
-                                <Input mb="1rem" type='text' name="name" />
-                                <FormLabel>E-mail</FormLabel>
-                                <Input mb="1rem" type='email' name="email" />
-                                <FormLabel>Password</FormLabel>
-                                <Input mb="1rem" type='password' name="password" />
-                                <Button type='submit'>Send</Button>
+                                <Input 
+                                    mb="1rem"
+                                    type='text'
+                                    name='nome'
+                                    placeholder='Nome'
+                                    required
+                                    border="1px solid"
+                                    bg="white"
+                                    borderRadius="25px"
+                                    height={50}
+                                    minWidth={250}
+                                />
+                                <Input 
+                                    mb="1rem"
+                                    type='email'
+                                    name='email'
+                                    placeholder='Email'
+                                    required
+                                    border="1px solid"
+                                    bg="white"
+                                    borderRadius="25px"  
+                                    height={50}
+                                    minWidth={250}
+                                />
+                                <Input 
+                                    mb="1rem"
+                                    type='password'
+                                    name='password'
+                                    placeholder='Senha'
+                                    required
+                                    border={`1px solid ${error || passwordError ? 'red' : 'gray'}`} // Bordas condicionais
+                                    bg="white"
+                                    borderRadius="25px"  
+                                    height={50}
+                                    minWidth={250}
+                                />
+                                <Input 
+                                    mb="1rem"
+                                    type='password'
+                                    name='password1'
+                                    placeholder='Confirme a senha'
+                                    required
+                                    border={`1px solid ${error || passwordError ? 'red' : 'gray'}`} // Bordas condicionais
+                                    bg="white"
+                                    borderRadius="25px"  
+                                    height={50}
+                                    minWidth={250}
+                                />
+                                {(error || passwordError) && (
+                                    <Text color="red.500" mb="1rem">
+                                        {error || passwordError}
+                                    </Text>
+                                )}
+                                <Button 
+                                    type='submit' 
+                                    size='lg' 
+                                    width={130} 
+                                    mx="auto" 
+                                    backgroundColor="#FFB210" 
+                                    display="block" 
+                                    borderRadius="25px" 
+                                >
+                                    Cadastrar
+                                </Button>
                             </FormControl>
                         </form>
                     </Center>
-                </Center>
+                </Flex>
             </Flex>
         </div>
     );
