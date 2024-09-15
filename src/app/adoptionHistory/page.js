9bad01f97b2fa2957bd501a8475b62ee857107c9
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2024-09-15 15:06:49 -0300

    feat: add animals component

--- src/app/adoptionHistory/page.js
+++ src/app/adoptionHistory/page.js
@@ -3,10 +3,12 @@ import "./styles.css";
 
 import Menu from '@/components/Menu';
 import HistoryCard from "@/components/HistoryCard";
+import Animals from "@/components/Animals";
 
 export default function AdoptionHistory() {
     return <div className="adoption-history-wrapper">
         <Menu/>
+        <Animals title="Patas e histórias"/>
         <div className='adoption-history'>
             <div className="history-cards">
                 <HistoryCard/>
