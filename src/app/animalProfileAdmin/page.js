44064170f1d17e193843de0de20c2e4c336f6d8e
viniciusbarbosa <vinicius.barbosa@ccc.ufcg.edu.br>
2024-09-18 02:12:32 -0300

    feat: Adding ProfileAdmin

---
+++ src/app/animalProfileAdmin/page.js
@@ -0,0 +1,31 @@
+"use client";
+import "./styles.css";
+
+import Menu from '@/components/Menu';
+import FormInput from "@/components/FormInput/FormInput";
+import FormSelect from "@/components/FormSelect/FormSelect";
+
+export default function animalProfileAdmin(){
+
+    return( 
+    <div>
+        <Menu/>
+        <img src="images/cat.png" alt="gato"/>
+        <input type="file"/>
+        <div className="register-grid">
+            <FormInput placeholder="Nome" contentEditable={false} sizeInput="small"/>
+            <FormInput placeholder="Raça" sizeInput="small"/>
+            <FormSelect defaultValue="Sexo" options={["Macho", "Fêmea"]}/>
+            <FormSelect defaultValue="Tipo" options={["Gato", "Cachorro", "Outro"]}/>
+            <FormSelect defaultValue="Estágio da vida" options={["Filhote", "Adulto", "Idoso"]}/>
+            <FormSelect defaultValue="É castrado ?" options={["Castrado", "Não castrado"]}/>
+            <FormSelect defaultValue="É vacinado ?" options={["Vacinado", "Não vacinado"]}/>
+        </div>
+        
+        <FormInput placeholder="Descrição" sizeInput="big" />
+        <button className="button-save">Salvar</button>
+
+        
+    </div>
+    );
+}
