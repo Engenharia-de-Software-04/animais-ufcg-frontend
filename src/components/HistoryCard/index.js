3827b9d2c134da322cf28c8f6e98609b4410716e
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2024-09-19 20:11:13 -0300

    feat: add page for adotion history with administrator options

--- src/components/HistoryCard/index.js
+++ src/components/HistoryCard/index.js
@@ -1,7 +1,7 @@
 "use client";
 import "./styles.css";
 
-export default function HistoryCard() {
+    export default function HistoryCard({ isAdmin }) {
     return (
         <div className="history-card">
             <div className="history-card-right">
@@ -10,6 +10,12 @@ export default function HistoryCard() {
             <div className="history-card-left">
                 <h2>Gatinha Nina</h2>
                 <p>Nina chegou tímida e assustada, mas logo se tornou carinhosa e brincalhona. Agora, é a dona do sofá e adora se esquentar ao sol, trazendo alegria e aconchego ao nosso lar com seu ronronar tranquilizador. Adotar a Nina foi como ganhar uma amiga fiel e especial. </p>
+                {isAdmin && (
+                    <div className="button-adoption-history-wrapper">
+                        <button className="button-adoption-history" type="button">Remover</button>
+                        <button className="button-adoption-history" type="button">Editar</button>
+                    </div>
+                )}
             </div>
         </div>
     )
