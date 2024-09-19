c0f9447904aafdec4d96982faf8a9ae7ed92b7de
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2024-09-18 22:24:03 -0300

    feat: adjusting style of input component

--- src/components/FormInput/FormInput.js
+++ src/components/FormInput/FormInput.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function FormInput({placeholder, contentEditable, sizeInput}) {
     return(
         <div>
-            <input type="text" className={`input-${sizeInput}`} placeholder={placeholder} contentEditable={contentEditable}/>
+            <input type="text" className={`generic-input input-${sizeInput}`} placeholder={placeholder} contentEditable={contentEditable}/>
         </div>
     );
     
