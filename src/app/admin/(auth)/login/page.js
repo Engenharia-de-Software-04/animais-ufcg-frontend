3a5bb0d0939228a5276dc9652fb79b2b07648120
larysamendess <larysa.almeida.ext@synchro.com.br>
2024-09-20 21:48:45 -0300

    fix: login messages

--- src/app/admin/(auth)/login/page.js
+++ src/app/admin/(auth)/login/page.js
@@ -1,12 +1,13 @@
 "use client";
 
-import React from 'react';
-import { Flex, Center, FormControl, FormLabel, Input, Button, Text, Image } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Flex, Center, FormControl, Input, Button, Text, Image } from '@chakra-ui/react';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
 export default function Login() {
     const router = useRouter();
+    const [error, setError] = useState('');
 
     const handleLoginAdmin = async (event) => {
         event.preventDefault();
@@ -21,27 +22,28 @@ export default function Login() {
             password: data["password"],
         });
 
-        if (result["status"] == 200) {
+        if (result.status === 200) {
             router.push("/admin");
+        } else if (result.status === 401) {
+            setError('Email ou senha incorreto, tente novamente');
         }
     };
 
     return (
         <div className="form-wrapper">
             <Flex
-            direction="column"
-            align="center"
-            justify="center"
-            h="100vh"
-            w="100vw"
-            bgImage="url('/images/plano_fundo.png')"
-            bgSize="cover"
-            bgPosition="center"
-            p="2rem"
+                direction="column"
+                align="center"
+                justify="center"
+                h="100vh"
+                w="100vw"
+                bgImage="url('/images/plano_fundo.png')"
+                bgSize="cover"
+                bgPosition="center"
+                p="2rem"
             >
-                
-                <Flex direction="column" align="center" p="2rem" maxWidth="550px"  boxShadow="none" border="none" mb="0">
-                    <Flex mb="2rem" align="center" >
+                <Flex direction="column" align="center" p="2rem" maxWidth="550px" boxShadow="none" border="none" mb="0">
+                    <Flex mb="2rem" align="center">
                         <Image src="/images/logo.png" alt="Logo" width={150} mr="1rem" />
                         <Text fontSize="3xl" fontWeight="bold" color="#FFB210">
                             Animais UFCG
@@ -50,13 +52,13 @@ export default function Login() {
                     <Center p="2rem">
                         <form onSubmit={handleLoginAdmin}>
                             <FormControl>
-                                
-                                <Input mb="1rem"
+                                <Input 
+                                    mb="1rem"
                                     type='email'
                                     name='email'
                                     placeholder='E-mail'
                                     required
-                                    border="1px solid"
+                                    border={`1px solid ${error ? 'red' : 'gray'}`}
                                     bg="white"
                                     borderRadius="25px"
                                     height={50}
@@ -67,12 +69,27 @@ export default function Login() {
                                     name='password'
                                     placeholder='Senha'
                                     required
-                                    border="1px solid"
+                                    border={`1px solid ${error ? 'red' : 'gray'}`}
                                     bg="white"
                                     borderRadius="25px"  
                                     height={50}
                                 />
-                                <Button type='submit' size='lg' width={130} mx="auto" backgroundColor="#FFB210" display="block" borderRadius="25px"  >Entrar</Button>
+                                {error && (
+                                    <Text color="red.500" mb="1rem">
+                                        {error}
+                                    </Text>
+                                )}
+                                <Button 
+                                    type='submit' 
+                                    size='lg' 
+                                    width={130} 
+                                    mx="auto" 
+                                    backgroundColor="#FFB210" 
+                                    display="block" 
+                                    borderRadius="25px"
+                                >
+                                    Entrar
+                                </Button>
                             </FormControl>
                         </form>
                     </Center>
