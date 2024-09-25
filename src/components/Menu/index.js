c43e5bdaf6e80d649577243fc895356be8c60ebb
karynaraujo <130762931+karynaraujo@users.noreply.github.com>
2024-09-25 16:51:11 -0300

    refactor: adding links to the menu and adjusting the menu size

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,80 +1,101 @@
 "use client";
 import "./styles.css";
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function Menu() {
-  const [menuBodyDisplay, setMenuBodyDisplay] = useState("none")
-  
+  const [menuBodyDisplay, setMenuBodyDisplay] = useState("none");
+
   const changeMenu = (event) => {
-    const target = event.currentTarget
+    const target = event.currentTarget;
 
     if (target.classList.contains("responsive-menu-opened")) {
-        target.classList.remove("responsive-menu-opened")
-        target.classList.add("responsive-menu-closed")
+      target.classList.remove("responsive-menu-opened");
+      target.classList.add("responsive-menu-closed");
 
-        setMenuBodyDisplay("block")
+      setMenuBodyDisplay("block");
     } else {
-        target.classList.remove("responsive-menu-closed")
-        target.classList.add("responsive-menu-opened")
-        
-        setMenuBodyDisplay("none")
-    }
-  }
+      target.classList.remove("responsive-menu-closed");
+      target.classList.add("responsive-menu-opened");
 
-  return <div>
-    <div className="menu-wrapper">
-        <a href="/">
-            <div className="logo"></div>
-        </a>
+      setMenuBodyDisplay("none");
+    }
+  };
+
+  return (
+    <div>
+      <div className="menu-wrapper">
+        <div className="logo">
+          <a href="/"></a>
+        </div>
         <nav className="main-nav">
-            <ul className="nav-wrapper">
-                <a>
-                    <li>Quem somos</li>
-                </a>
-                <a href="/animalList" >
-                    <li>Adoção</li>
-                </a>
-                <a href="/adoptionHistory">
-                    <li>Relatos</li>
-                </a>
-                <a href="https://www.instagram.com/animaisufcg/">
-                    <li>Contato</li>
-                </a>
-            </ul>
+          <ul className="nav-wrapper">
+            <a href="/aboutUs">
+              <li>Quem somos</li>
+            </a>
+            <a href="/animalList">
+              <li>Adoção</li>
+            </a>
+            <a href="/adoptionHistory">
+              <li>Relatos</li>
+            </a>
+            <a href="https://www.instagram.com/animaisufcg/">
+              <li>Contato</li>
+            </a>
+          </ul>
         </nav>
         <nav className="help-nav">
-            <a href="/donationAndHelp">
-                <li>Ajuda</li>
-            </a>
+          <a href="/donationAndHelp">
+            <li>Ajuda</li>
+          </a>
         </nav>
-        <div onClick={changeMenu} className="responsive-menu responsive-menu-opened">
-            <div className="line line1"></div>
-            <div className="line line2"></div>
-            <div className="line line3"></div>
+        <div
+          onClick={changeMenu}
+          className="responsive-menu responsive-menu-opened"
+        >
+          <div className="line line1"></div>
+          <div className="line line2"></div>
+          <div className="line line3"></div>
         </div>
-        <div className="responsive-menu-body" style={{ display: menuBodyDisplay }}>
-            <nav>
-                <ul className="responsive-menu-items">
-                    <a>
-                        <li><span className="material-symbols-outlined">forum</span> Quem somos</li>
-                    </a>
-                    <a href="/animalList">
-                        <li><span className="material-symbols-outlined">pets</span> Adoção</li>
-                    </a>
-                    <a href="/adoptionHistory">
-                        <li><span className="material-symbols-outlined">import_contacts</span> Relatos</li>
-                    </a>
-                    <a href="https://www.instagram.com/animaisufcg/">
-                        <li><span className="material-symbols-outlined">call</span> Contato</li>
-                    </a>
-                </ul>
-            </nav>
-            <nav className="responsive-help-nav">
-                <a href="/donationAndHelp">
-                    <li>Ajuda</li>
-                </a>
-            </nav>
+        <div
+          className="responsive-menu-body"
+          style={{ display: menuBodyDisplay }}
+        >
+          <nav>
+            <ul className="responsive-menu-items">
+              <a href="/aboutUs">
+                <li>
+                  <span className="material-symbols-outlined">forum</span> Quem
+                  somos
+                </li>
+              </a>
+              <a href="/animalList">
+                <li>
+                  <span className="material-symbols-outlined">pets</span> Adoção
+                </li>
+              </a>
+              <a href="/adoptionHistory">
+                <li>
+                  <span className="material-symbols-outlined">
+                    import_contacts
+                  </span>{" "}
+                  Relatos
+                </li>
+              </a>
+              <a href="https://www.instagram.com/animaisufcg/">
+                <li>
+                  <span className="material-symbols-outlined">call</span>{" "}
+                  Contato
+                </li>
+              </a>
+            </ul>
+          </nav>
+          <nav className="responsive-help-nav">
+            <a href="/donationAndHelp">
+              <li>Ajuda</li>
+            </a>
+          </nav>
         </div>
+      </div>
     </div>
-  </div>;
+  );
 }
